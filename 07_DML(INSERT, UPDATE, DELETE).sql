cb95eee8b3a72d30725dd223f24e1c2934811a53
Darkiller1234 <kyoung8292@naver.com>
2024-08-13 14:06:00 +0900

    0813

---
+++ 07_DML(INSERT, UPDATE, DELETE).sql
@@ -0,0 +1,16 @@
+/*
+    DQL(QUERY 데이터 정의어) : SELECT
+    DML(MANIPULATION 데이터 조작어) : INSERT, UPDATE, DELETE
+    DDL(DEFINITION 데이터 정의어) : CREATE, ALTER, DROP
+    DCL(CONTROL 데이터 제어어) : GRANT, REVOKE
+    TCL(TRANSACTION 트랜잭션 제어어) : COMMIT, ROLLBACK
+    
+    <DML>
+    데이터 조작어
+    테이블 값을 삽입(INSERT), 수정(UPDATE), 삭제(DELETE)하는 구문
+*/
+
+/*
+    1. INSERT
+
+*/
